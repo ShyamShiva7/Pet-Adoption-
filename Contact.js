780e82899ea9ee9f18dd481be8c068e7bb97ad54
ShyamShiva7 <166889511+ShyamShiva7@users.noreply.github.com>
2025-01-09 09:03:13 +0530

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,31 @@
+import React from "react";
+import developerPng from "./images/developer-png.png";
+
+const Contact = () => {
+  return (
+    <div className="contactUs-main-container">
+      <div className="contactUs-left-para">
+        <h3>Let's get in touch</h3>
+        <i class="fa fa-envelope"></i>
+        <a class="mail-links" href="mailto:shyam@gmail.com">
+          petzen5@gmail.com
+        </a>
+
+        <i class="fa fa-instagram"></i>
+        <a class="mail-links" href="#">
+          @kaxhie_x
+        </a>
+
+        <i class="fa fa-phone"></i>
+        <a class="mail-links" href="tel:+923019583959">
+          +92 301 9583959
+        </a>
+      </div>
+      <div className="contactUs-pic">
+        <img src={developerPng} alt="Profile"/>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Pet-Adoption-full stack
+
